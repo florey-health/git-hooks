69cb1b160883bb8e2b8671ce13fac651679d7e8d
Torben Sko <torben.sko@florey.com.au>
2017-03-10 16:36:18 +1100

    Used the scripts directory to correctly set the paths

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SRC="./hooks/"
-DEST="./.git/hooks/"
+DEST="$( pwd )/.git/hooks/"
+SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 
 if [ -d "$DEST" ]; then
   if [ -d "$SRC" ]; then
-    cp "${SRC}*" $DEST
+    cp "${SRC}"* $DEST
     echo "copied hooks to .git directory"
   else
     echo "unable to find hooks directory"
